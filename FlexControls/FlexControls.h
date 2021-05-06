c9ee82b93d14214f1f7d5f85ad920e04f03130f5
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-06 10:45:43 +0200

    Initial Commit

---
+++ FlexControls/FlexControls.h
@@ -0,0 +1,18 @@
+//
+//  FlexControls.h
+//  FlexControls
+//
+//  Created by Martin Rehder on 06.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlexControls.
+FOUNDATION_EXPORT double FlexControlsVersionNumber;
+
+//! Project version string for FlexControls.
+FOUNDATION_EXPORT const unsigned char FlexControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexControls/PublicHeader.h>
+
+
